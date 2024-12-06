76887f91f92a3235e73a7e65a053b74d5c53f401
Roberto Beraldo <robertoberaldobr@gmail.com>
2024-12-06 10:20:16 -0300

    melhorar testes

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -1,39 +1,27 @@
 <?php
 
-test('test Posts index', function () {
-    $response = $this->get(route('posts.index'));
+use Illuminate\Testing\TestResponse;
 
-    $response->assertStatus(200);
-
-    // Assert response is JSON
+function assertValidJsonResponse(TestResponse $response): void
+{
     $response->assertHeader('Content-Type', 'application/json');
-
-    // Assert JSON structure and content
     $response->assertJsonStructure([
         'data',
         'success',
     ]);
-
     $response->assertJson([
         'success' => true,
     ]);
+}
+
+test('test Posts index', function () {
+    $response = $this->get(route('posts.index'));
+    $response->assertStatus(200);
+    assertValidJsonResponse($response);
 });
 
 test('test Posts store', function () {
     $response = $this->post(route('posts.store'));
-
     $response->assertStatus(201);
-
-    // Assert response is JSON
-    $response->assertHeader('Content-Type', 'application/json');
-
-    // Assert JSON structure and content
-    $response->assertJsonStructure([
-        'data',
-        'success',
-    ]);
-
-    $response->assertJson([
-        'success' => true,
-    ]);
+    assertValidJsonResponse($response);
 });
